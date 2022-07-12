83a9513560f0ece5f8446e54d038491d7f299a9e
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-07-12 14:14:23 -0500

    changed the api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,80 @@
-const router = require("express").Router();
+const fs = require("fs");
+const { dirname } = require("path");
+const path = require("path");
+const getNotes = require("../db/db.json");
+console.log(process.pid);
 
 //request notes
-module.exports = (app) => {
-  //get request
-  app.get("api/notes/:id", (req, res) => {
-    res.json(notes[req.params.id]);
+module.exports = function (app) {
+  //take json array to db.json
+  function writeData(notes) {
+    notes = JSON.stringify(notes);
+    console.log(notes);
+    fs.writeFilySync("./db/db.json", notes, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+  app.get("/api/notes", function (req, res) {
+    res.json(getNotes);
   });
 
-  app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-      var notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });
+  //   app.get("/api/notes", (req, res) => {
+  //     fs.readFile("./db/db.json", "utf8", (err, data) => {
+  //       if (err) throw err;
+  //       var notes = JSON.parse(data);
+  //       res.json(notes);
+  //     });
+  //   });
   //Get api notes should read the db.json file and return all saved notes as json.
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
-  });
+  //   app.get("/notes", (req, res) => {
+  //     res.sendFile(path.join(__dirname, "./db/db.json"));
+  //   });
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"));
-  });
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.join(__dirname, "/index.html"));
+  //   });
   //post should recieve a new note to save on the request body,
-  app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-      var notes = JSON.parse(data);
-      let userNote = req.body;
-      userNote.id = Math.floor(Math.random() * 5000);
-      notes.push(userNote);
-      fs.writeFile("./db/db.json", JSON.stringify(notes), (err, data) => {
-        res.json(userNote);
-      });
-    });
+  app.post("/api/notes", function (req, res) {
+    if (getNotes.length == 0) {
+      req.body.id = "0";
+    } else {
+      const lastItemIndex = getNotes.length - 1;
+      const lastItem = getNotes[lastItemIndex];
+      const newId = JSON.parse(lastItem.id) + 1;
+      req.body.id = JSON.stringify(newId);
+    }
+    console.log("req.body.id: " + req.body.id);
+
+    getNotes.push(req.body);
+    writeToDB();
+    console.log(getNotes);
+
+    res.json(req.body);
   });
-  //delete /api/notes/:id should receive a query parameter containing the id of a note to deleteNote.
-  app.delete("/api/notes/:id", (req, res) => {
-    let db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    //     if (err) throw err;
-    //     let notes = JSON.parse(data);
-    //     const newNotes = notes.filter(
-    //       (note) => note.id !== parseInt(req.params.id)
-    //     );
-    let deleteNotes = db.filter((item) => item.id !== req.params.id);
-    //file system to writeFile//rewriting to db.json
-    fs.writeFileSync(
-      "/db/db.json",
-      JSON.stringify(deleteNotes),
-      (err, data) => {
-        res.json(deleteNotes);
+  function writeToDB() {
+    fs.writeFile(
+      path.join(__dirname, "..", "db/db.json"),
+      JSON.stringify(getNotes, "\t"),
+      (err) => {
+        if (err) throw err;
+        return true;
       }
     );
+  }
+  //delete /api/notes/:id should receive a query parameter containing the id of a note to deleteNote.
+  app.delete("/api/notes/:id", (req, res) => {
+    let id = req.params.id.toString();
+    for (i = 0; i < getNotes.length; i++) {
+      if (getNotes[i].id == id) {
+        console.log("Successfully deleted!");
+        res.send(getNotes[i]);
+
+        getNotes.splice(i, 1);
+        break;
+      }
+    }
+    writeData(getNotes);
   });
 };
